327bfdbdcf8d583ee5695eaf14acf3cd6b98bc6d
ayoubft <ayoubfatihi1999@gmail.com>
2022-01-26 02:31:06 +0100

    Fixed info

--- js/script.js
+++ js/script.js
@@ -111,14 +111,10 @@ info.onAdd = function (map) {
 // method that we will use to update the control based on feature properties passed
 info.update = function (props) {
   this._div.innerHTML =
-    "<h4>Cases</h4>" +
+    "<h4>Covid-19 (Omicron?)</h4>" +
     (props
-      ? "<b>" +
-        props.Nom_Province +
-        "</b><br />" +
-        props[day] +
-        " cas confirmés </sup>"
-      : "Hover over a state");
+      ? `<b>${props.Nom_Province}</b><br />${props[day]} Confirmed Cases </sup>`
+      : "Hover over a province!");
 };
 
 info.addTo(map);
